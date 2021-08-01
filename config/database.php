5a2fd066fe85d0b9fefa86f7fc3fc592f7f99569
hashammage-io <hashammage-io@github.com>
2021-08-01 23:14:11 +0500

    dataase

--- config/database.php
+++ config/database.php
@@ -50,7 +50,7 @@ return [
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'lowlowergone'),
             'username' => env('DB_USERNAME', 'lowlowergone'),
-            'password' => env('DB_PASSWORD', 'Standard12345!'),
+            'password' => env('DB_PASSWORD', 'Standard12345/1'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
