a4dcb02579c09e1dc79dd3740866b35036a2250f
hashammage-io <hashammage-io@github.com>
2021-08-01 22:57:07 +0500

    cros

--- resources/views/user/buyer/layouts/app.blade.php
+++ resources/views/user/buyer/layouts/app.blade.php
@@ -4,6 +4,7 @@
      <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
     <meta name="description" content="low lower gone reverse auction site .">
     <meta name="keywords" content="low lower gone reverse auction classified ad site">
     <meta name="author" content="lowlowergone">
